ce5c2ad0c2c0ab38677642192261779183435a2b
Sapar Jumabekov <58820192+SapaTech@users.noreply.github.com>
2021-11-03 15:46:38 +0300

    add FRFirebaseSignUser

---
+++ Sources/FRFirebase/Extensions/StringExtension.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by user on 03.11.2021.
+//
+
+import Foundation
+
+extension String {
+    
+    // MARK: - Data
+    
+    /**
+     JSBoost: Wrapper of emtry string "".
+     */
+    static var empty: String { return "" }
+    
+}
--- Sources/FRFirebase/Models/FRFirebaseSignUser.swift
+++ Sources/FRFirebase/Models/FRFirebaseSignUser.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public enum FRFirebaseSignType {
+    case google
+    case facebook
+    case apple
+    case phone(_ smsCode: String, _ verificationID: String)
+    case customPhone(_ token: String, _ phoneNumber: String)
+}
+
+public struct FRFirebaseSignUser {
+    var uid: String
+    var type: FRFirebaseSignType
+    var name: String?
+    var email: String?
+    var phone: String?
+}
