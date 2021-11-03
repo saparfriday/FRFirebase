825a6a4cdb3a6ae059c0c90ddc66969f90e39653
Sapar Jumabekov <58820192+SapaTech@users.noreply.github.com>
2021-11-03 15:58:33 +0300

    remove duplicate

--- Sources/FRFirebase/Models/FRFirebaseSignUser.swift
+++ Sources/FRFirebase/Models/FRFirebaseSignUser.swift
@@ -7,14 +7,6 @@
 
 import Foundation
 
-public enum FRFirebaseSignType {
-    case google
-    case facebook
-    case apple
-    case phone(_ smsCode: String, _ verificationID: String)
-    case customPhone(_ token: String, _ phoneNumber: String)
-}
-
 public struct FRFirebaseSignUser {
     var uid: String
     var type: FRFirebaseSignType
