a10a3d4201f2d7182c00118941fc7970930bd010
Sapar Jumabekov <58820192+SapaTech@users.noreply.github.com>
2021-11-03 15:38:12 +0300

    add FirebaseFirestoreSwift-Beta

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
             dependencies: [
                 .product(name: "FirebaseAuth", package: "Firebase"),
                 .product(name: "FirebaseFirestore", package: "Firebase"),
-                .product(name: "FirebaseAnalyticsSwift-Beta", package: "Firebase"),
+                .product(name: "FirebaseFirestoreSwift-Beta", package: "Firebase"),
                 .product(name: "FacebookLogin", package: "Facebook"),
                 "GoogleSignIn"
             ]
